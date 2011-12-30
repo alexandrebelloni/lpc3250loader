a14af9547a33fc67a890e4db88864c6fc39998a5
Alexandre Belloni <alexandre.belloni@piout.net>
2011-12-30 11:56:43 +0100

    Better program structure
    Signed-off-by: Alexandre Belloni <alexandre.belloni@piout.net>

--- LPC3250loader.py
+++ LPC3250loader.py
@@ -20,164 +20,171 @@
 
 
 import sys, os, serial, getopt
-
-loadaddr = 0x00000000
-imgaddr = 0x80000004
-
-serialport = "/dev/ttyUSB0"
-burnerfile = None
-imagefile = None
-
-
-def usage():
-    print "%s programfile" % (sys.argv[0])
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "a:i:l:p:hv", ["help", "imgaddress=", "image=", "loadaddress=", "port="])
-except getopt.GetoptError, err:
-    print str(err)
-    usage()
-    sys.exit(2)
+    
 verbose = False
 
-if len(args) != 1:
-    usage()
-    sys.exit()
+def usage():
+    #TODO
+    print "%s programfile" % (sys.argv[0])
 
-burnerfile = args[0]
 
-for o, a in opts:
-    if o == "-v":
-        verbose = True
-    elif o in ("-h", "--help"):
+def serialread(ser):
+    global verbose
+    c = ser.read()
+    if verbose:
+        print "Got %c(%d)" % (c, ord(c))
+    return c
+
+
+# Send a 32bits in 4 bytes
+def send32(ser, val):
+    for i in range(4):
+        ser.write(chr(val&0xFF))
+        val = val >> 8
+
+
+# Send a file byte per byte
+def sendfile(ser, filename):
+    f = open(filename, "rb")
+    try:
+        byte = f.read(1)
+        while byte != "":
+            ser.write(byte)
+            byte = f.read(1)
+    finally:
+        f.close()
+
+
+# Send an image file to a burner
+def sendimage(ser, filename, addr):
+    # Burner is getting executed
+    print "Waiting for burner..."
+    input = serialread(ser)
+    if input != 'X':
+        print "error 4"
+        sys.exit()
+
+    ser.write('p')
+    print "OK. Sent char p"
+
+    print "Sending Image address: 0x%x" % (addr)
+    send32(ser, addr)
+
+    size = os.path.getsize(filename)
+    print "Sending Filesize: %d" % (size)
+    send32(ser, size)
+
+    input = serialread(ser)
+    if input != 'o':
+        print "error 5"
+        sys.exit()
+
+    sendfile(ser, filename)
+
+    input = serialread(ser)
+    if input != 't':
+        print "error 6"
+        sys.exit()
+
+    print "Image file sent, flashing..."
+
+
+def main():
+    global verbose
+    burnerfile = None
+    imagefile = None
+    serialport = "/dev/ttyUSB0"
+    loadaddr = 0x00000000
+    imgaddr = 0x80000004
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "a:i:l:p:hv", ["help", "imgaddress=", "image=", "loadaddress=", "port="])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
+
+    if len(args) != 1:
         usage()
         sys.exit()
-    elif o in ("-i", "--image"):
-        imagefile = a
-    elif o in ("-p", "--port"):
-        serialport = a
-    #TODO addresses
-    else:
-        usage()
+
+    burnerfile = args[0]
+
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-i", "--image"):
+            imagefile = a
+        elif o in ("-p", "--port"):
+            serialport = a
+        elif o in ("-l", "--loadaddress"):
+            loadaddr = int(a, 16)
+        elif o in ("-a", "--imgaddress"):
+            imgaddr = int(a, 16)
+        else:
+            usage()
+            sys.exit()
+
+    if not os.path.exists(burnerfile):
+        sys.stderr.write("Couldn't find " + burnerfile + "\n")
+        sys.exit(1)
+
+    if imagefile and not os.path.exists(imagefile):
+        sys.stderr.write("Couldn't find " + imagefile + "\n")
+        sys.exit(1)
+
+    ser = serial.Serial(serialport, 115200, timeout=300)
+
+    print "Waiting for BOOTID..."
+
+    while True:
+        input = serialread(ser)
+        if input == '5':
+            break
+
+    ser.write('A')
+    print "Found! Sent char A"
+
+    print "Waiting for 2nd BOOTID..."
+    input = 0
+    input = serialread(ser)
+    if input != '5':
+        print "error 2"
         sys.exit()
 
-print "%s, %s, %s" % (burnerfile, imagefile, serialport)
+    ser.write('U3')
+    print "Found! Sent chars U3"
 
-if not os.path.exists(burnerfile):
-    sys.stderr.write("Couldn't find " + burnerfile + "\n")
-    sys.exit(1)
+    print "Waiting for confirmation..."
 
-if imagefile and not os.path.exists(imagefile):
-    sys.stderr.write("Couldn't find " + imagefile + "\n")
-    sys.exit(1)
+    input = 0
+    input = serialread(ser)
+    if input != 'R':
+        print "error 3"
+        sys.exit()
 
-ser = serial.Serial(serialport, 115200, timeout=300)
+    print "Found! Sent chars U3"
 
-print "Waiting for BOOTID..."
+    print "Sending load address: 0x%x" % (loadaddr)
+    send32(ser, loadaddr)
 
-while True:
-    input = ser.read()
-    print "Got %c(%d)" % (input, ord(input))
-    if input == '5':
-        break
+    size = os.path.getsize(burnerfile)
+    print "Filesize: %d" % (size)
+    send32(ser, size)
 
-ser.write('A')
-print "Found! Sent char A"
+    sendfile(ser, burnerfile)
 
+    if imagefile:
+        sendimage(ser, imagefile, imgaddr)
 
-print "Waiting for 2nd BOOTID..."
-
-input = 0
-input = ser.read()
-print "Got %c(%d)" % (input, ord(input))
-if input != '5':
-    print "error 2"
-    sys.exit()
-
-ser.write('U3')
-print "Found! Sent chars U3"
-
-print "Waiting for confirmation..."
-
-input = 0
-input = ser.read()
-print "Got %c(%d)" % (input, ord(input))
-if input != 'R':
-    print "error 3"
-    sys.exit()
-
-print "Found! Sent chars U3"
-
-print "Sending load address: %d" % (loadaddr)
-val = loadaddr
-for i in range(4):
-    ser.write(chr(val&0xFF))
-    val = val >> 8
-
-size = os.path.getsize(burnerfile)
-print "Filesize: %d" % (size)
-val = size
-for i in range(4):
-    ser.write(chr(val&0xFF))
-    val = val >> 8
+    print "press (Ctrl+C) to quit"
+    while True:
+        input = ser.readline()
+        print input
 
 
-f = open(burnerfile, "rb")
-try:
-    byte = f.read(1)
-    while byte != "":
-        ser.write(byte)
-        byte = f.read(1)
-finally:
-    f.close()
-
-# Burner is getting executed
-print "Waiting for burner..."
-input = ser.read()
-print "Got %c(%d)" % (input, ord(input))
-if input != 'X':
-    print "error 4"
-    sys.exit()
-
-ser.write('p')
-print "OK. Sent char p"
-
-print "Sending Image address: %d" % (imgaddr)
-val = imgaddr
-for i in range(4):
-    ser.write(chr(val&0xFF))
-    val = val >> 8
-
-size = os.path.getsize(imagefile)
-print "Sending Filesize: %d" % (size)
-val = size
-for i in range(4):
-    ser.write(chr(val&0xFF))
-    val = val >> 8
-
-input = ser.read()
-print "Got %c(%d)" % (input, ord(input))
-if input != 'o':
-    print "error 5"
-    sys.exit()
-
-f = open(imagefile, "rb")
-try:
-    byte = f.read(1)
-    while byte != "":
-        ser.write(byte)
-        byte = f.read(1)
-finally:
-    f.close()
-
-input = ser.read()
-print "Got %c(%d)" % (input, ord(input))
-if input != 't':
-    print "error 6"
-    sys.exit()
-
-print "Everything OK, flashing... press (Ctrl+C) to quit"
-while True:
-    input = ser.readline()
-    print input
-
+if __name__ == "__main__":
+    main()
